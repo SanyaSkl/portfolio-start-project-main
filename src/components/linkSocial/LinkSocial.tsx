d6b2e442efe444e2b11b781b6271fabffa4101b7
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-03-31 17:43:58 +0300

    added LinkSocial_Styles and map method created

--- src/components/linkSocial/LinkSocial.tsx
+++ src/components/linkSocial/LinkSocial.tsx
@@ -1,50 +1,36 @@
 import React from 'react';
 import {Icon} from "../icon/Icon";
-import styled from "styled-components";
-import {theme} from "../../styles/Theme";
+import {S} from './LinkSocial_Styles';
 
+const socialItemsData = [
+    {
+        iconId: "socialIconGit"
+    },
+    {
+        iconId: "socialIconTwitter"
+    },
+    {
+        iconId: "socialIconLinkedIn"
+    },
 
+]
 
 export const LinkSocial = () => {
     return (
-        <SocialList>
-            <SocialItem>
-                <SocialLink href={"#"}>
-                    <Icon height={"32px"} width={"32px"} viewBox={"0 0 32px 32px"} iconId={"socialIconGit"}/>
-                </SocialLink>
-            </SocialItem>
-            <SocialItem>
-                <SocialLink href={"#"}>
-                    <Icon height={"32px"} width={"32px"} viewBox={"0 0 32px 32px"} iconId={"socialIconTwitter"}/>
-                </SocialLink>
-            </SocialItem>
-            <SocialItem>
-                <SocialLink href={"#"}>
-                    <Icon height={"32px"} width={"32px"} viewBox={"0 0 32px 32px"} iconId={"socialIconLinkedIn"}/>
-                </SocialLink>
-            </SocialItem>
-        </SocialList>
+        <S.SocialList>
+            {socialItemsData.map((s, index) => {
+                return (
+                    <S.SocialItem key={index}>
+                        <S.SocialLink href={"#"}>
+                            <Icon height={"32px"}
+                                  width={"32px"}
+                                  viewBox={"0 0 32px 32px"}
+                                  iconId={s.iconId}/>
+                        </S.SocialLink>
+                    </S.SocialItem>
+                )
+            })}
+        </S.SocialList>
     );
 };
-
-const SocialList = styled.ul`
-  display: flex;
-  gap: 10px;
-  
-  @media ${theme.media.tablet} {
-    padding-left: 20px;
-  }
-
-  @media ${theme.media.mobile} {
-    margin: 0 auto;
-  }
-`
-
-const SocialItem = styled.li`
-
-`
-
-const SocialLink = styled.a`
-
-`
 
---
+++ src/components/linkSocial/LinkSocial_Styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import {theme} from "../../styles/Theme";
+
+const SocialList = styled.ul`
+  display: flex;
+  gap: 10px;
+
+  @media ${theme.media.tablet} {
+    padding-left: 20px;
+  }
+
+  @media ${theme.media.mobile} {
+    margin: 0 auto;
+  }
+`
+
+const SocialItem = styled.li`
+
+`
+
+const SocialLink = styled.a`
+
+`
+
+export const S = {
+    SocialLink, SocialItem, SocialList
+}

36e832525dbdea8be34cd262c223a804467c6887
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-03-31 13:51:56 +0300

    added Menu_Styles

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,51 +1,18 @@
 import React from 'react';
-import styled from "styled-components";
-import {theme} from "../../styles/Theme";
+import {S} from './Menu_Styles';
 
 
 export const Menu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
     return (
-        <StyledMenu>
+        <S.StyledMenu>
             <ul>
                 {props.menuItems.map((item, index) => {
-                    return <ListItem key={index}>
-                        <Link href="#">{item}</Link>
-                    </ListItem>
+                    return <S.ListItem key={index}>
+                        <S.Link href="#">{item}</S.Link>
+                    </S.ListItem>
                 })}
             </ul>
-        </StyledMenu>
+        </S.StyledMenu>
     );
 };
 
-const StyledMenu = styled.nav`
-  display: flex;
-  margin-left: auto;
-  justify-content: center;
-  padding-left: 50px;
-
-  ul {
-    display: flex;
-    gap: 50px;
-    padding-right: 50px;
-  }
-
-  a {
-    color: ${theme.colors.menu};
-  }
-`
-
-const ListItem = styled.li`
-
-`
-
-const Link = styled.a`
-  color: ${theme.colors.menu};
-  font-family: DM Sans, sans-serif;
-  font-size: 20px;
-  letter-spacing: 0;
-  text-align: center;
-  
-  &:hover{
-    color: ${theme.colors.accent};
-  }
-`
---
+++ src/components/menu/Menu_Styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import {theme} from "../../styles/Theme";
+
+const StyledMenu = styled.nav`
+  display: flex;
+  margin-left: auto;
+  justify-content: center;
+  padding-left: 50px;
+
+  ul {
+    display: flex;
+    gap: 50px;
+    padding-right: 50px;
+  }
+
+  a {
+    color: ${theme.colors.menu};
+  }
+`
+
+const ListItem = styled.li`
+
+`
+
+const Link = styled.a`
+  color: ${theme.colors.menu};
+  font-family: DM Sans, sans-serif;
+  font-size: 20px;
+  letter-spacing: 0;
+  text-align: center;
+  
+  &:hover{
+    color: ${theme.colors.accent};
+  }
+`
+
+export const S = {
+    Link, ListItem, StyledMenu
+}
